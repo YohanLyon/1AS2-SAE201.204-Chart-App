243ff5ac51d77dedb1bccd2076bc619ce5ba8b16
LAPIERRE Yohan <105936245+LapierreYohan@users.noreply.github.com>
2022-06-13 17:32:49 +0200

    Add files via upload

---
+++ SAE201-204/src/sae201204/SAE201204.java
@@ -0,0 +1,14 @@
+
+package sae201204;
+
+public class SAE201204 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+     
+        Fenetre win = new Fenetre();
+        win.setVisible(true);
+    }
+}
