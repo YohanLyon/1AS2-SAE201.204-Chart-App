fa46858e03c7e8141c5f84172bef09cd0911632c
LAPIERRE Yohan <105936245+LapierreYohan@users.noreply.github.com>
2022-06-15 09:42:55 +0200

    Add files via upload

---
+++ SAE201_204_g2/Sae201204/src/sae201204/PassWord.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sae201204;
+
+/**
+ *
+ * @author p2103678
+ */
+public class PassWord {
+    public static String password = "12103678";
+}
---
+++ SAE201_204_g2/Sae201204/src/sae201204/Sae201204.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sae201204;
+
+import java.sql.SQLException;
+
+/**
+ *
+ * @author p2103678
+ */
+public class Sae201204 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        
+        Fenetre win = new Fenetre();
+        win.setVisible(true);
+    }
+    
+}
